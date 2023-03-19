8a993f5b234d18e167ce7940d682a1a586b11fca
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-19 18:56:07 -0400

    all tests passed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -8,7 +11,10 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
-    }
-}
+    public Integer selection(Integer startIndex, String selection) {
+        //create an arrayList from the array to utilize the methods
+        ArrayList<String> musicSelection = new ArrayList<String>(Arrays.asList(this.playList));
+        int forwardCount = Math.abs(musicSelection.indexOf(selection) - startIndex);
+        int backwardsCount = Math.abs((musicSelection.size()+ startIndex) -musicSelection.lastIndexOf(selection));
+        return Math.min(forwardCount, backwardsCount);
+    }}
